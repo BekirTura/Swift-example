b2041ddf1bae1a422c460f57e0eb5b2c288dca4b
bekir <bekirtura1238@hotmail.com>
2014-09-28 22:17:06 +0300

    animation

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   
     @IBOutlet weak var alien: UIImageView!
     var counter=1
+    var timer = NSTimer()
     @IBAction func updateImage(sender: AnyObject) {
        counter++
         // let frame2=UIImage(named: "frame2.png")
@@ -24,11 +25,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
+    func result(){
+        counter++
+        alien.image = UIImage(named: "frame\(counter).png")
+        if(counter==5){
+            counter = 1
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func viewDidLayoutSubviews() {
+       
+    }
+    override func viewDidDisappear(animated: Bool) {
+        UIView.animateWithDuration(1, animations:{
+         
+        })
     }
 
 
