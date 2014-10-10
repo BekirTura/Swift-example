d8cb1308ead5f7ab8ba9c267e7b7661119c3fda7
bekir <bekirtura1238@hotmail.com>
2014-10-11 00:02:06 +0300

    Advance Learning Maps

--- LearningMaps/AppDelegate.swift
+++ LearningMaps/AppDelegate.swift
@@ -1,24 +1,28 @@
 //
 //  AppDelegate.swift
-//  LearningMaps
+//  Maps
 //
-//  Created by bekir tura on 07/10/14.
-//  Copyright (c) 2014 bekir tura. All rights reserved.
+//  Created by Bekir TURA on 10/10/2014.
 //
 
 import UIKit
 
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate{
+                            
     var window: UIWindow?
+    
+   
 
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
-
+    
+  
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- LearningMaps/ViewController.swift
+++ LearningMaps/ViewController.swift
@@ -1,69 +1,184 @@
 //
 //  ViewController.swift
-//  LearningMaps
+//  Learning Maps
 //
-//  Created by bekir tura on 07/10/14.
-//  Copyright (c) 2014 bekir tura. All rights reserved.
+//  Created by Bekir Tura on 10/10/2014.
 //
 
 import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController ,MKMapViewDelegate ,CLLocationManagerDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
-    
-    var manager = CLLocationManager()
-    
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var myMap: MKMapView!
+    
+    
+    @IBAction func findMe(sender: AnyObject) {
         
-        // core location
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
         
+        
+    }
+    
+    @IBOutlet var latitude : UILabel!
+    @IBOutlet var longitude : UILabel!
+    @IBOutlet var address : UILabel!
+    @IBOutlet var altitude : UILabel!
+    @IBOutlet var speed : UILabel!
+    @IBOutlet var heading : UILabel!
+    
+    var manager:CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var latitude:CLLocationDegrees = 40.748655
-        var longtitude:CLLocationDegrees = -73.985621
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if activePlace == -1 {
+            
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+            
+        } else {
+        
+        
+        let lat = NSString(string: places[activePlace]["lat"]!).doubleValue
+        
+        let lon = NSString(string: places[activePlace]["lon"]!).doubleValue
+        
+        var latitude:CLLocationDegrees = lat
+        
+        var longitude:CLLocationDegrees = lon
+        
         var latDelta:CLLocationDegrees = 0.01
+        
         var lonDelta:CLLocationDegrees = 0.01
+        
         var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
         var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        mapView.setRegion(region, animated: true)
+        
+        myMap.setRegion(region, animated: true)
+            
         var annotation = MKPointAnnotation()
+            
         annotation.coordinate = location
-        annotation.title = "Statue of Liberty "
-        annotation.subtitle = " One day I'll go here ..."
-        mapView.addAnnotation(annotation)
-        var uilpgr = UILongPressGestureRecognizer(target: self, action: "Action :")
+            
+        annotation.title = places[activePlace]["name"]
+            
+        myMap.addAnnotation(annotation)
+            
+            
+            
+        }
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
         uilpgr.minimumPressDuration = 2.0
-        mapView.addGestureRecognizer(uilpgr)
+        
+        myMap.addGestureRecognizer(uilpgr)
+        
         
     }
-    func action(gestureRecognizer:UIGestureRecognizer){
-        println("dslmldnv,")
-        var touchPoint = gestureRecognizer.locationInView(self.mapView)
-        var newCoordinate:CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView:self.mapView)
-        var newAnnotation = MKPointAnnotation()
-        newAnnotation.coordinate = newCoordinate
-        newAnnotation.title = "new Point"
-        newAnnotation.subtitle = " One day I'll go here ..."
-        mapView.addAnnotation(newAnnotation)
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        
+        
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            
+        
+        var touchPoint = gestureRecognizer.locationInView(self.myMap)
+        
+        var newCoordinate = myMap.convertPoint(touchPoint, toCoordinateFromView: self.myMap)
+        
+        
+        
+        var loc = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+        
+        
+        CLGeocoder().reverseGeocodeLocation(loc, completionHandler:{(placemarks, error) in
+            
+            if ((error) != nil)  { println("Error: \(error)") }
+            else {
+                
+                
+                
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                var subThoroughfare:String
+                var thoroughfare:String
+                
+                if ((p.subThoroughfare) != nil) {
+                    subThoroughfare = p.subThoroughfare
+                } else {
+                    subThoroughfare = ""
+                }
+                
+                if ((p.thoroughfare) != nil) {
+                    thoroughfare = p.thoroughfare
+                } else {
+                    thoroughfare = ""
+                }
+                
+                
+                var annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                
+                var title = "\(subThoroughfare) \(thoroughfare)"
+                
+                if title == " " {
+                    
+                    var date = NSDate.date()
+                    
+                    title = "Added \(date)"
+                    
+                }
+                
+                annotation.title = title
+                
+                self.myMap.addAnnotation(annotation)
+                
+                places.append(["name":title,"lat":"\(newCoordinate.latitude)","lon":"\(newCoordinate.longitude)"])
+                
+                
+            }
+            
+            
+            })
+            
+            
+        }
+
         
     }
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    
+    override func viewWillDisappear(animated: Bool) {
+        
+        self.navigationController?.navigationBarHidden = false
+        
+    }
+    
+    
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
         
         var userLocation:CLLocation = locations[0] as CLLocation
         
         var latitude:CLLocationDegrees = userLocation.coordinate.latitude
         
-        var longtitude:CLLocationDegrees = userLocation.coordinate.longitude
+        var longitude:CLLocationDegrees = userLocation.coordinate.longitude
         
         var latDelta:CLLocationDegrees = 0.01
         
@@ -71,25 +186,49 @@ class ViewController: UIViewController ,MKMapViewDelegate ,CLLocationManagerDele
         
         var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
         
-        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
         
         var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         
-        mapView.setRegion(region, animated: true)
+        myMap.setRegion(region, animated: true)
         
+        manager.stopUpdatingLocation()
         
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:{(placemarks, error) in
+            
+            if ((error) != nil)  { println("Error: \(error)") }
+            else {
+                
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                var subThoroughfare:String
+                
+                if ((p.subThoroughfare) != nil) {
+                    subThoroughfare = p.subThoroughfare
+                } else {
+                    subThoroughfare = ""
+                }
+                
+                self.address.text =  "\(subThoroughfare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \(p.country)"
+                
+                
+                
+                
+            }
+            
+            
+            })
         
-    }
-    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
-        println(error)
     }
     
-
+   
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
